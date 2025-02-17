8940cf4e742b80fbb5e437f7facc23e1e25e6cf1
robertlcc <robertlcc7@gmail.com>
2025-02-17 11:03:31 +0800

    [Homework2] HW02_1, HW02_2

--- student_assignment.py
+++ student_assignment.py
@@ -1,13 +1,39 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import (CharacterTextSplitter,
-                                      RecursiveCharacterTextSplitter)
+from langchain_text_splitters import CharacterTextSplitter,RecursiveCharacterTextSplitter
 
 q1_pdf = "OpenSourceLicenses.pdf"
 q2_pdf = "勞動基準法.pdf"
 
-
 def hw02_1(q1_pdf):
-    pass
+    # Load the PDF file
+    pdf_loader = PyPDFLoader(q1_pdf)
+    documents = pdf_loader.load()
+
+    # Use CharacterTextSplitter to split the document by pages
+    text_splitter = CharacterTextSplitter(separator='\f', chunk_size=1000, chunk_overlap=0)
+    #chunks_by_pages = text_splitter.split_text(document_text)
+    last_document = text_splitter.split_documents(documents)
+
+    return last_document[-1]
+  
 
 def hw02_2(q2_pdf):
-    pass
+    # Load the PDF file
+    pdf_loader = PyPDFLoader(file_path=q2_pdf)
+    documents = pdf_loader.load()
+
+    # Combine the document text into a single string
+    document_text = "\n".join([doc.page_content for doc in documents])
+
+    # Use RecursiveCharacterTextSplitter to split the document by pages
+    recursive_splitter = RecursiveCharacterTextSplitter(
+        separators=[
+            r"第\s+[一二三四五六七八九十]+\s+章",
+            r"第\s+[0-9]+(?:-[0-9]+)?\s+條"
+        ],
+        chunk_size=0,
+        chunk_overlap=0,
+        is_separator_regex = True)
+    chunks_by_pages_recursive = recursive_splitter.split_text(document_text)
+
+    return len(chunks_by_pages_recursive)
